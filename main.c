9a9fbfd36a4cf1fbac35bbad1a877db32064a64e
Pavel Petrovic <pavel.petrovic@gmail.com>
2018-05-28 22:42:21 +0200

    a* search, and constructing the map based on measurements (pretty-print map using UTF-8 graphical characters)

--- main.c
+++ main.c
@@ -1,11 +1,51 @@
+#include <stdlib.h>
 #include "tatrabot.h"
 #include "demo.h"
 
-static int16_t sn[16][16];
-static int16_t so[16][16];
+#define MAP_OBSTACLE_THRESHOLD 3
 
-static int16_t map[16][16];
-static int16_t visited[16][16];
+#define TICK_DISTANCE_IN_CM 0.640625
+
+static int8_t fake_map[17][17] = {
+  { 0, 0, 0, 5, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 5, 0, 5, 5, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 5, 0, 5, 0, 0, 5, 0, 0, 5, 0, 0, 0, 0, 0 },
+  { 0, 0, 5, 0, 0, 5, 0, 0, 5, 0, 0, 5, 0, 0, 0, 0, 0 },
+  { 0, 0, 5, 0, 5, 5, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0 },
+  { 0, 0, 5, 0, 0, 5, 5, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0 },
+  { 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0 },
+  { 0, 5, 5, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 5, 0, 0, 0, 0, 5, 0, 0, 5, 5, 5, 0, 0, 0, 0, 0 },
+  { 0, 5, 0, 0, 5, 5, 5, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0 },
+  { 0, 5, 0, 5, 5, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0 },
+  { 0, 5, 0, 0, 5, 0, 0, 5, 5, 5, 5, 5, 5, 5, 0, 0, 0 },
+  { 0, 5, 5, 0, 5, 0, 0, 5, 5, 0, 0, 5, 0, 5, 0, 0, 0 },
+  { 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 5, 0, 5, 0, 0, 0 },
+  { 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 0, 0 },
+  { 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0 },
+  { 0, 0, 0, 0, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+
+
+static int16_t sn[17][17];
+static int16_t so[17][17];
+
+static int8_t map[17][17];
+static int8_t visited[17][17];
+static int8_t steps_from_start[17][17];
+static int8_t came_from[17][17];
+static int8_t map[17][17];
+
+static int8_t queue_row[16*16];
+static int8_t queue_col[16*16];
+
+static int16_t queue_start;
+static int16_t queue_end;
+
+static int8_t plan_row[16*16];
+static int8_t plan_col[16*16];
+static int8_t plan_length;
+
 
 static int8_t robot_row, robot_col;
 static int8_t dx, dy;
@@ -14,22 +54,23 @@ static int8_t dx, dy;
 #define DO_NOT_SHOW_THE_ROBOT 0
 
 
-static void init_fake_map(void)
+static void prepare_fake_map(void)
 {
-  map[8][5] = 1;
-  map[8][4] = 1;
+  for (int r = 0; r < 17; r++)
+    for (int c = 0; c < 17; c++)
+      map[r][c] = fake_map[r][c];
 }
 
 static void follow_line_to_crossing(int rotations, int measuring)
 {
   int measured = 0;
   countB=0;
-  while (((senseLine() & 1) == 0) && (countB < rotations))
+  while (((senseLine() & 1) == 0) && (countB < (unsigned int)rotations))
   {
       if (measuring)
       {
         measured++;
-//        sense();
+        sense(countB);
       }
       // if the second from the left sees the line, a little bit to the right
       if (((senseLine() >> 2) & 1) == 1)
@@ -52,6 +93,7 @@ static void follow_line_to_crossing(int rotations, int measuring)
   if (measured)
   {
 //    chprintf(BT, "Measured %d times!\n", measured);
+    updateMap();
   }
 }
 
@@ -101,33 +143,6 @@ static void right(void){
   robot_col -= dx;
 
   return;
-
-    //countA countB
-      // if the second from the left sees the line, a little bit to the right
-      //if (((senseLine() >> 2) & 1) && ((senseLine() >> 1) & 1) && ((senseLine() >> 0) & 1) && (((senseLine() >> 3)&1) == 0))
-      /*{
-        setMotor(3, 0);
-      }*/
-      while ((senseLine() & 1) == 0)
-      {
-          // if the second from the left sees the line, a little bit to the right
-          if (((senseLine() >> 2) & 1) == 1)
-          {
-            setMotor(2, -5000);
-            setMotor(1, 0);
-          }
-          // otherwise if the leftmost sees no line, a little bit to the left
-          else if (((senseLine() >> 3) & 1) == 0)
-          {
-            setMotor(1, -5000);
-            setMotor(2, 0);
-          }
-          else setMotor(3, -4000);
-      }
-
-  setMotor(3, -5000);
-  chThdSleepMilliseconds(200);
-  setMotor(3, 0);
 }
 
 static void left(void){
@@ -136,7 +151,7 @@ static void left(void){
   chThdSleepMilliseconds(500);
   follow_line_to_crossing(17, 0);
   setMotor(3, 0);
-  setMotor(1, -4000);
+  setMotor(1, -5500);
   countB=0;
 
   while (countB < 25){
@@ -173,6 +188,7 @@ static void left(void){
 #define KEY_RIGHT  'd'
 #define KEY_FUN 'f'
 #define KEY_PRINT 'p'
+#define KEY_MAPPRINT 'P'
 #define KEY_MEASURE 'm'
 #define KEY_TRAVEL 't'
 
@@ -272,7 +288,28 @@ void sensor_vector(int sensor, int *sdx, int *sdy)
   }
 }
 
-void update_boxes_unseen_with_delta(int sensor, int distance, int delta_r)
+void correct_position_with_movement(int *x, int *y, uint32_t distance)
+{
+  if (dx == 0) {
+    if (dy < 0) {
+      // Hore
+      *y = *y + distance;
+    } else {
+      // Dole
+      *y = *y - distance;
+    }
+  } else {
+    if (dx < 0) {
+      // Dolava
+      *x = *x - distance;
+    } else {
+      // Doprava
+      *x = *x + distance;
+    }
+  }
+}
+
+void update_boxes_unseen_with_delta(int sensor, int distance, int delta_r, uint32_t distanceY)
 {
   int sdx, sdy;
   sensor_vector(sensor, &sdx, &sdy);
@@ -288,6 +325,7 @@ void update_boxes_unseen_with_delta(int sensor, int distance, int delta_r)
     while (in_cone) {
       float x = r*box_width + sensor_shifts[sensor][0];
       float y = d + sensor_shifts[sensor][1];
+      correct_position_with_movement(&x, &y, distance);
       if (!is_in_cone(x, y) && !is_in_cone(x + box_width, y)) {
         in_cone = 0;
       } else {
@@ -306,7 +344,7 @@ void update_boxes_unseen_with_delta(int sensor, int distance, int delta_r)
   }
 }
 
-void update_boxes_seen_with_delta(int sensor, int distance, int delta_r)
+void update_boxes_seen_with_delta(int sensor, int distance, int delta_r, uint32_t distanceY)
 {
   int sdx, sdy;
   sensor_vector(sensor, &sdx, &sdy);
@@ -319,6 +357,7 @@ void update_boxes_seen_with_delta(int sensor, int distance, int delta_r)
   while (in_cone) {
     float x = r*box_width + sensor_shifts[sensor][0];
     float y = (delta - 1) * box_width + sensor_shifts[sensor][1];
+    correct_position_with_movement(&x, &y, distance);
     if (!overfill_cone(x, y) && !is_in_cone(x, y) && !is_in_cone(x + box_width, y)) {
       in_cone = 0;
 //      chprintf(BT, "DONE X:%d Y:%d\n\r", (int) (10000 * x), (int) (10000 * y));
@@ -335,30 +374,39 @@ void update_boxes_seen_with_delta(int sensor, int distance, int delta_r)
   }
 }
 
-void update_boxes_unseen(int sensor, int distance)
+void update_boxes_unseen(int sensor, int distance, uint32_t distanceY)
 {
-  update_boxes_unseen_with_delta(sensor, distance, 1);
-  update_boxes_unseen_with_delta(sensor, distance, -1);
+  update_boxes_unseen_with_delta(sensor, distance, 1, distanceY);
+  update_boxes_unseen_with_delta(sensor, distance, -1, distanceY);
 }
 
-void update_boxes_seen(int sensor, int distance)
+void update_boxes_seen(int sensor, int distance, uint32_t distanceY)
 {
-  update_boxes_seen_with_delta(sensor, distance, 1);
-  update_boxes_seen_with_delta(sensor, distance, -1);
+  update_boxes_seen_with_delta(sensor, distance, 1, distanceY);
+  update_boxes_seen_with_delta(sensor, distance, -1, distanceY);
 }
 
 // pri vzdialenosti 100 CM, od stredu vidi do cone s odchylkou 15 CM do oboch stran
-void sense(void)
+void sense(uint32_t rotates)
 {
+  uint32_t distanceY = rotates * TICK_DISTANCE_IN_CM;
   for(int i = 0; i < 3; i++){
     int sense_temp = measure_distance(i);
     if (sense_temp != 1000){
-      update_boxes_seen(i, sense_temp);
-      update_boxes_unseen(i, sense_temp);
+      update_boxes_seen(i, sense_temp, distanceY);
+      update_boxes_unseen(i, sense_temp, distanceY);
     } else{
-      update_boxes_unseen(i, 200);
+      update_boxes_unseen(i, 200, distanceY);
     }
   }
+}
+
+void heart_beat(int kind)
+{
+  sound(200 + kind * 200);
+  chThdSleepMilliseconds(5);
+  nosound();
+  chThdSleepMilliseconds(250);
 }
 
 char readKey(void)
@@ -366,6 +414,7 @@ char readKey(void)
   char key;
   do {
     key = keyPressed(BT);
+    heart_beat(9);
   } while (key == 0);
   return key;
 }
@@ -409,6 +458,76 @@ void print_array(uint8_t show_robot)
   chprintf(BT, "row=%d, col=%d, dx=%d, dy=%d\n\r", robot_row, robot_col, dx, dy);
 }
 
+int is_on_the_plan(int r, int c)
+{
+  for (int i = 0; i < plan_length; i++)
+    if ((plan_row[i] == r) && (plan_col[i] == c)) return 1;
+  return 0;
+}
+
+
+void print_level_of_gray(int level)
+{
+  switch (level)
+  {
+  case 0: chprintf(BT, " ");
+         break;
+  case 1: chprintf(BT, ".");
+         break;
+  case 2: chprintf(BT, ":");
+         break;
+  case 3: chprintf(BT, "o");
+         break;
+  case 4: chprintf(BT, "O");
+         break;
+  case 5: chprintf(BT, "@");
+         break;
+  case 6: chprintf(BT, "%c%c%c", 0xE2, 0x96, 0x91);
+         break;
+  case 7: chprintf(BT, "%c%c%c", 0xE2, 0x96, 0x92);
+         break;
+  case 8: chprintf(BT, "%c%c%c", 0xE2, 0x96, 0x93);
+         break;
+  case 9:
+  case 10:
+    chprintf(BT, "%c%c%c", 0xE2, 0x96, 0x88);
+         break;
+  case 20: chprintf(BT, "x");
+           break;
+  }
+}
+
+
+
+void print_map(uint8_t show_robot)
+{
+  chprintf(BT, "-----map----------------\n\r");
+  for (int row = 0; row < 16; row++)
+  {
+    for (int col = 0; col < 16; col++)
+    {
+       if (show_robot && (row == robot_row) && (col == robot_col))
+          chprintf(BT, " R%c ", robot_orientation());
+       else
+       {
+          print_level_of_gray(map[row][col]);
+          print_level_of_gray(map[row][col]);
+          print_level_of_gray(map[row][col]);
+          if (is_on_the_plan(row, col))
+          {
+            chprintf(BT, "*");
+          }
+          else
+          {
+            print_level_of_gray(map[row][col]);
+          }
+       }
+    }
+    chprintf(BT, "\n\r");
+  }
+  chprintf(BT, "row=%d, col=%d, dx=%d, dy=%d\n\r", robot_row, robot_col, dx, dy);
+}
+
 
 void fun(void)
 {
@@ -432,40 +551,234 @@ void fun(void)
   }
 }
 
-void measure(void)
+
+void updateMap(void)
 {
-  sense();
-}
-
-void navigate_to(int drow, int dcol)
-{
-  // travel from location robot_row, robot_col with heading dx,dy
-
-  //  to location drow, dcol, avoiding non-zero elements in map[][]
-  for (int row = 0; row < 16; row++)
-     for (int col = 0; col < 16; col++)
-        visited[row][col] = 0;
-
-  int r = robot_row;
-  int c = robot_col;
-
-/*
-  while ((r != drow) || (c != dcol))
-  {
-    visited[r][c] = 1;  // 1 = decrease row, 2 = increase col, 3 = decrease col, 4 = increase row
-    if ()
-  }
-
   for (int row = 0; row < 16; row++)
     {
       for (int col = 0; col < 16; col++)
       {
-         robot_row
-         robot_col
-      }
+        int sum = sn[row][col] + so[row][col];
+        if (sum == 0)
+          map[row][col] = 20;
+        else
+        map[row][col] = 10*so[row][col]/sum;
 
+      }
     }
-  if*/
+}
+
+void measure(void)
+{
+  sense(0);
+  heart_beat(3);
+  updateMap();
+  heart_beat(5);
+}
+
+
+void initialize_plan_data_structures(void)
+{
+  for (int row = 0; row < 16; row++)
+     for (int col = 0; col < 16; col++)
+        visited[row][col] = 0;
+
+  queue_row[0] = robot_row;
+  queue_col[0] = robot_col;
+  visited[robot_row][robot_col] = 1;
+  queue_start = 0;
+  queue_end = 1;
+
+}
+
+void insert_into_queue(int from_start, int r, int c, int drow, int dcol)
+{
+  int value = from_start + abs(drow - r) + abs(dcol - c);
+  int i;
+  for (i = queue_start; i < queue_end; i++)
+  {
+    if (value < steps_from_start[queue_row[i]][queue_col[i]] + abs(queue_row[i] - drow) + abs(queue_col[i] - dcol))
+      break;
+  }
+  for (int j = queue_end; j > i; j--)
+  {
+    queue_row[j] = queue_row[j - 1];
+    queue_col[j] = queue_col[j - 1];
+  }
+  queue_row[i] = r;
+  queue_col[i] = c;
+  queue_end++;
+}
+
+
+int8_t dir[4][2] = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} };
+
+int create_plan(int drow, int dcol)
+{
+   int current_row, current_col;
+
+   initialize_plan_data_structures();
+   while (queue_end > queue_start)
+   {
+     current_row = queue_row[queue_start];
+     current_col = queue_col[queue_start];
+     queue_start++;
+     //chprintf(BT, "Q:%d,%d\r\n", current_row, current_col);
+
+     for (int try_direction = 0; try_direction < 4; try_direction++)
+     {
+       int8_t candidate_row = current_row + dir[try_direction][0];
+       int8_t candidate_col = current_col + dir[try_direction][1];
+       //chprintf(BT, "  C:%d,%d\r\n", candidate_row, candidate_col);
+
+       if ((candidate_row < 0) || (candidate_row > 15) ||
+           (candidate_col < 0) || (candidate_col > 15))
+         continue;
+       if (map[candidate_row][candidate_col] > MAP_OBSTACLE_THRESHOLD)
+         continue;
+       if (visited[candidate_row][candidate_col])
+         continue;
+       //chprintf(BT, "  ok %d, %d\r\n", drow, dcol);
+       visited[candidate_row][candidate_col] = 1;
+       steps_from_start[candidate_row][candidate_col] = steps_from_start[current_row][current_col] + 1;
+       came_from[candidate_row][candidate_col] = try_direction;
+
+       if ((candidate_row == drow) && (candidate_col == dcol))
+       {
+         queue_start = queue_end;
+         break;
+       }
+
+       //chprintf(BT, "  ok %d,%d\r\n", queue_start, queue_end);
+       insert_into_queue(steps_from_start[candidate_row][candidate_col],
+                         candidate_row, candidate_col, drow, dcol);
+     }
+   }
+   if (!visited[drow][dcol]) return 0;
+   int step = steps_from_start[drow][dcol];
+   current_row = drow;
+   current_col = dcol;
+   while (step >= 0)
+   {
+      plan_row[step] = current_row;
+      plan_col[step] = current_col;
+      step--;
+      int going_to = (came_from[current_row][current_col] + 2) % 4;
+      //chprintf(BT, "[%d,%d,%d,%d,%d] - ", current_row, current_col, going_to, dir[going_to][0], dir[going_to][0] );
+      current_row = current_row + dir[going_to][0];
+      current_col = current_col + dir[going_to][1];
+
+   }
+   plan_length = steps_from_start[drow][dcol];
+   //chprintf(BT, "plan len: %d\r\n", plan_length);
+   return 1;
+}
+
+void perform_movement(int ddy, int ddx)
+{
+  if (dx == 0) {
+    if (dy < 0) {
+      // Hore
+      if (ddy == 1) {
+        left();
+        left();
+      } else if (ddy == -1) {
+        // OK
+      } else if (ddx == 1) {
+        right();
+      } else if (ddx == -1) {
+        left();
+      }
+    } else {
+      // Dole
+      if (ddy == 1) {
+        // OK
+      } else if (ddy == -1) {
+        left();
+        left();
+      } else if (ddx == 1) {
+        left();
+      } else if (ddx == -1) {
+        right();
+      }
+    }
+  } else {
+    if (dx < 0) {
+      // Dolava
+      if (ddy == 1) {
+        right();
+      } else if (ddy == -1) {
+        left();
+      } else if (ddx == 1) {
+        left();
+        left();
+      } else if (ddx == -1) {
+        // OK
+      }
+    } else {
+      // Doprava
+      if (ddy == 1) {
+        left();
+      } else if (ddy == -1) {
+        right();
+      } else if (ddx == 1) {
+        // OK
+      } else if (ddx == -1) {
+        left();
+        left();
+      }
+    }
+  }
+  forward();
+}
+
+int not_visited(int r, int c)
+{
+  if (r < 0 || r >= 17 || c < 0 || c >= 17) {
+    return 0;
+  }
+  return !visited[r][c];
+}
+
+void execute_plan(void)
+{
+  for (int row = 0; row < 16; row++)
+  {
+    for (int col = 0; col < 16; col++)
+      visited[row][col] = 0;
+  }
+
+  while (true) {
+    if (is_on_the_plan(robot_row - 1, robot_col) && not_visited(robot_row - 1, robot_col)) {
+      visited[robot_row - 1][robot_col] = 1;
+      perform_movement(-1, 0);
+      // chod zo svojej polohy po mape HORE
+    } else if (is_on_the_plan(robot_row + 1, robot_col) && not_visited(robot_row + 1, robot_col)) {
+      visited[robot_row + 1][robot_col] = 1;
+      perform_movement(1, 0);
+      // chod zo svojej polohy po mape DOLE
+    } else if (is_on_the_plan(robot_row, robot_col - 1) && not_visited(robot_row, robot_col - 1)) {
+      visited[robot_row][robot_col - 1] = 1;
+      perform_movement(0, -1);
+      // chod zo svojej polohy po mape DOLAVA
+    } else if (is_on_the_plan(robot_row, robot_col + 1) && not_visited(robot_row, robot_col + 1)) {
+      visited[robot_row][robot_col + 1] = 1;
+      perform_movement(0, 1);
+      // chod zo svojej polohy po mape DOPRAVA
+    } else {
+      break;
+    }
+  }
+}
+
+
+void navigate_to(int drow, int dcol)
+{
+  prepare_fake_map();
+  if (create_plan(drow, dcol))
+    execute_plan();
+  else
+    chprintf(BT, "No trajectory exists.\r\n");
 }
 
 void processKey(char key)
@@ -486,10 +799,13 @@ void processKey(char key)
       chprintf(BT, " Distance unit %d!\n\r", DISTANCE_UNIT);
       break;
     case KEY_SENSE:
-      while (true) sense();
+      while (true) sense(0);
       break;
     case KEY_PRINT:
       print_array(SHOW_THE_ROBOT);
+      break;
+    case KEY_MAPPRINT:
+      print_map(SHOW_THE_ROBOT);
       break;
     case KEY_MEASURE:
       measure();
@@ -516,13 +832,12 @@ void processKey(char key)
   }
 }
 
-
 int main(void)
 {
   tatrabotInit();
 
   robot_row = 15;
-  robot_col = 8;
+  robot_col = 9;
 
   dx = 0;
   dy = -1;
